d6f680e73dae05a99783cf453a1ee0cbacb8f073
Himanshushri11 <shrivastavahimanshu194@gmail.com>
2025-04-20 11:24:51 +0530

    20-04-2025 version

--- src/Pages/StudentDashboard.jsx
+++ src/Pages/StudentDashboard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const StudentDashboard = () => {
   return (
     <div>
-      <h1>Hello Welcome to teacher desboard....</h1>
+      <h1>Hello Welcome to student desboard....</h1>
       <p>we are create for </p>
       <Link to="/profile">
   <button className="btn">Go to Profile</button>
